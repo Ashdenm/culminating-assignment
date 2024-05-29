17f1b8cc92f59afc49d3cb6bbf2855243a0b064d
Ashden M <162188847+Ashdenm@users.noreply.github.com>
2024-05-29 13:45:48 -0400

    initial commit

---
+++ JavaCulminating/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        shop f = new shop();
+        f.store();
+    }
+}
---
+++ JavaCulminating/src/shop.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class shop {
+    public static int money;
+
+    public void store() {
+        JOptionPane.showMessageDialog(null, "welcome to the shop");
+
+    } 
+
+}
