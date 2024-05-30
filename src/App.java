d3f530a4e9e88268d9c32ce3780008366436b4ce
Ashden M <162188847+Ashdenm@users.noreply.github.com>
2024-05-30 14:15:07 -0400

    backup

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        shop f = new shop();
-        f.store();
+        JOptionPane.showMessageDialog(null, "");
     }
 }
---
+++ src/character.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class character {
+
+
+    public void pc() {
+
+
+    } 
+
+}
