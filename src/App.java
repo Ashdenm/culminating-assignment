214f9a8af424a14f0911473ad3a48b69b974cf43
Ashden M <162188847+Ashdenm@users.noreply.github.com>
2024-05-30 15:07:48 -0400

    adding code for artificer class

--- src/App.java
+++ src/App.java
@@ -1,7 +1,19 @@
 import javax.swing.JOptionPane;
 
 public class App {
+    static Object[] classes = {"Artificer", "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
+
+    static String title = "Character Creator";
+    static String message = "Choose a Class";
+
     public static void main(String[] args) throws Exception {
-        JOptionPane.showMessageDialog(null, "");
+        String userClass = (String)JOptionPane.showInputDialog(null,
+        message, title, JOptionPane.PLAIN_MESSAGE, null,  classes, "Artificer");
+
+        if (userClass == "Artificer") {
+            
+        }
+
+            
     }
 }
--- src/artificer.java
+++ src/artificer.java
@@ -1,7 +1,6 @@
 import javax.swing.JOptionPane;
 
-public class character {
-
+public class artificer {
 
     public void pc() {
 
